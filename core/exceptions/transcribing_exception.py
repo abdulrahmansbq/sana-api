a891868155313924fccb5384cab414bf5b9671ef
abdulrahmansbq <abdulrahmansbq@gmail.com>
2024-11-01 13:42:32 +0300

    upload transcribing service

---
+++ core/exceptions/transcribing_exception.py
@@ -0,0 +1,10 @@
+class TranscribingException(Exception):
+    """
+    Exception raised for errors that occur in transcribing service class
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}'
