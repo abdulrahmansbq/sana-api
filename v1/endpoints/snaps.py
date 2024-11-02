c3d9157a9245aa0ae49a1264930ca6741e50d3a9
abdulrahmansbq <abdulrahmansbq@gmail.com>
2024-11-02 17:45:01 +0300

    Add the snaps endpoints

---
+++ v1/endpoints/snaps.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Form
+from core.controllers.chat_controller import ChatController
+from core.controllers.snap_controller import SnapController
+
+router = APIRouter(
+    prefix="/snaps",
+    tags=["snaps"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/generate")
+async def snap_generator(namespace_id: str = Form(...), namespace_type: str = Form(...), transcript: str = Form(...)):
+    snaps  = await SnapController(namespace_id, transcript, namespace_type).generate()
+    return {"status": "Success", "message": snaps}
